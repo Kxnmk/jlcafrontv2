dcf0acd6b2cc02cde38c92350a95e8705b976da3
Gabriel Lopez <gabriellopez@MacBook-Pro-de-Gabriel.local>
2018-07-04 13:49:12 -0500

    min fix

--- src/app/layout/demandas/timeline/tLine.component.ts
+++ src/app/layout/demandas/timeline/tLine.component.ts
@@ -42,7 +42,6 @@ export class TLineComponent implements OnInit {
                         data => {
                             if (data.length !== 0) {
                                 this.status = data;
-                                console.log(data);
                             } else {
                                 this.toastr.error('Error al obtener informacion');
 
